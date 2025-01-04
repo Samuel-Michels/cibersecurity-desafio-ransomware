7d2b2b75a89d62b8d8c94b54fe5b467c4d08a894
Samuel_Michels <77849693+Samuel-Michels@users.noreply.github.com>
2025-01-04 18:07:39 -0300

    Melhorias no código de criptografia e descriptografia de arquivos
    Utilização do contexto `with` para abertura e fechamento seguros de arquivos. - Adição de tratamento de exceções para capturar e exibir erros durante os processos de criptografia e descriptografia. - Modularização do código em funções `encrypt_file` e `decrypt_file` para melhor organização e reutilização. - Remoção dos arquivos originais após a criptografia e descriptografia. - Criação dos arquivos criptografados e descriptografados com nomes adequados.

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,31 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
+def decrypt_file(file_name, key):
+    try:
+        # Abrir o arquivo criptografado
+        with open(file_name, "rb") as file:
+            file_data = file.read()
+
+        # Descriptografar o arquivo
+        aes = pyaes.AESModeOfOperationCTR(key)
+        decrypt_data = aes.decrypt(file_data)
+
+        # Remover o arquivo criptografado
+        os.remove(file_name)
+
+        # Criar o arquivo descriptografado
+        new_file_name = file_name.replace(".ransomwaretroll", "")
+        with open(new_file_name, "wb") as new_file:
+            new_file.write(decrypt_data)
+
+        print(f"Arquivo '{new_file_name}' descriptografado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao descriptografar o arquivo: {e}")
+
+# Nome do arquivo e chave de descriptografia
 file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
-
-## chave para descriptografia
 key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
-os.remove(file_name)
-
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+# Chamar a função de descriptografia
+decrypt_file(file_name, key)
